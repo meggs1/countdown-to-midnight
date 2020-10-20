6ce56b0120e5f2f969f4f71ca80e8053cbab1587
meggs1 <megan.schertz@gmail.com>
2020-10-20 16:39:05 -0500

    commit tree 4d6dcc478a8e60e6eab4023d69bd05b24625d901

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(int)
+    int = 10
+    while int > 0
+        puts "#{int} SECOND(S)!"
+    int -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+    sleep(5)
 end
